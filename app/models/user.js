076183aca018f1e4f17b403f14bd9dd23179d5c9
shkuropat <e.shkuropat@gmail.com>
2012-06-12 17:18:06 +0400

    Removed unused files

--- app/models/user.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose'),
-    Schema   = mongoose.Schema;
-
-
-var User = module.exports = mongoose.model('User', new Schema({
-  username: String,
-  password: String
-}));
--- server/admin/user/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-/*global nodeca*/
-
-module.exports = function (params, next) {
-  console.dir(nodeca.config.menus);
-  next();
-};
