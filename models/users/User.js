b59aebae55530971a5258f3183aa0dabb8f65d8b
shkuropat <e.shkuropat@gmail.com>
2012-06-28 21:40:05 +0400

    Added user model

---
+++ models/users/User.js
@@ -0,0 +1,55 @@
+"use strict";
+
+/*global nodeca*/
+
+var Crypto = require('crypto');
+var mongoose = nodeca.runtime.mongoose;
+var Schema = mongoose.Schema;
+
+var cache = {
+    post_count        : { type: Number, default: 0 }
+
+  , last_visit_ts     : Date
+  , last_viset_ip     : String
+
+  , warning_points    : { type: Number, default: 0 }
+  , banned_till_ts    : Date
+
+  , userpic_version   : { type: Number, default: 0 }
+  , avatar_version    : { type: Number, default: 0 }
+};
+
+var User = module.exports.User = new mongoose.Schema({
+
+    // user-friendly id (autoincremented)
+    id                : { type: Number, required: true, min: 1, index: true }
+
+  , first_name        : String
+  , last_name         : String
+  , login             : String
+  , password          : String
+  , email             : String
+
+  , joined_ts         : Date
+
+  , primary_group     : Schema.ObjectId
+  , secondary_groups  : [Schema.ObjectId]
+
+  , cache             : cache
+});
+
+User.statics.fitchByIdList = function (id_list, callback) {
+  this.find({_id:{$in:id_list}}, callback);
+};
+
+var hash = function(password, salt) {
+  return Crypto.createHmac('sha256', salt).update(password).digest('hex');
+};
+
+User.methods.setPass = function(password) {
+  this.password = hash(password, password + this.joined_ts);
+};
+
+module.exports.__init__ = function __init__() {
+  return mongoose.model('users.User', User);
+};
