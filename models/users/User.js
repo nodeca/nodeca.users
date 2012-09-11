e5f167faed71bba56e97bf75e695b6c83194d9ce
Eugene Shkuropat <e.shkuropat@gmail.com>
2012-09-11 15:21:34 +0400

    Fixed user model

--- models/users/User.js
+++ models/users/User.js
@@ -10,13 +10,11 @@ var Schema = mongoose.Schema;
 var User = module.exports.User = new mongoose.Schema({
 
     // user-friendly id (autoincremented)
-    id                : { type: Number, required: true, min: 1, index: true }
+    id                : { type: Number, min: 1, index: true }
 
   , first_name        : String
   , last_name         : String
   , nick              : String
-  , password          : String
-  , email             : String
 
   , joined_ts         : Date
 
@@ -29,20 +27,28 @@ var User = module.exports.User = new mongoose.Schema({
   , _post_count       : { type: Number, default: 0 }
 
   , _last_visit_ts    : Date
-  , _last_viset_ip    : String
+  , _last_visit_ip    : String
 
   , _warning_points   : { type: Number, default: 0 }
   , _banned_till_ts   : Date
 });
 
 
-var hash = function(password, salt) {
-  return Crypto.createHmac('sha256', salt).update(password).digest('hex');
+User.statics.new_session = function(user_id, ts, ip, callback) {
+  this.findOne({ _id: user_id })
+      .exec(function(err, user) {
+    if (err) {
+      callback(err);
+      return;
+    }
+    user._last_visit_ts = ts;
+    user._last_visit_ip = ip;
+    user.save(function(err) {
+      callback(err, user);
+    });
+  });
 };
 
-User.methods.setPass = function(password) {
-  this.password = hash(password, password + this.joined_ts);
-};
 
 module.exports.__init__ = function __init__() {
   return mongoose.model('users.User', User);
