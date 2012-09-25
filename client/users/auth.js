f62945c2b3a4aa06d84170d879514980a528f69a
Eugene Shkuropat <varnak@debian.RT-G32>
2012-09-25 19:31:56 +0400

    Add realtime nick checking

--- client/users/auth.js
+++ client/users/auth.js
@@ -101,3 +101,37 @@ module.exports.register = function ($form, event) {
   });
   return false;
 };
+
+
+/**
+ *  client.common.auth.check_nick($elem, event)
+ *
+ *  send nick value on server
+ *  and show error if nick exists
+ **/
+module.exports.check_nick = function($elem, event) {
+  var nick = $elem.val();
+  nodeca.server.users.auth.register.check_nick({nick: nick}, function(err, request){
+    var $group = $elem.parents('.control-group:first');
+    var message;
+    if (err) {
+      if (err.statusCode === 409) {
+        $group.addClass('error');
+
+        message = err.message['nick'];
+        $elem.parent().find('.help-block').text(message);
+        return;
+      }
+      message = nodeca.runtime.t('common.notice.internal_server_error');
+      nodeca.client.common.notice.show(message);
+      return;
+    }
+    if ($group.hasClass('error')) {
+      $group.removeClass('error');
+
+      message = nodeca.runtime.t('users.auth.reg_form.nick_help');
+      $elem.parent().find('.help-block').text(message);
+    }
+  });
+  return false;
+};
---
+++ server/users/auth/register/check_nick.js
@@ -0,0 +1,45 @@
+"use strict";
+
+/*global nodeca, _*/
+
+var User = nodeca.models.users.User;
+
+// Validate input parameters
+//
+var params_schema = {
+  nick: {
+    type: "string",
+    minLength: 1,
+    required: true
+  }
+};
+nodeca.validate(params_schema);
+
+/**
+ * users.auth.register.check_nick(params, callback) -> Void
+ *
+ * ##### Params
+ * - nick(String):        Nickname
+ *
+ * Register new user
+ *
+ **/
+module.exports = function (params, next) {
+  var env = this;
+  User.findOne({ 'nick': params.nick}).setOptions({ lean: true })
+      .exec(function(err, doc){
+    if (err) {
+      next(err);
+      return;
+    }
+    if (!_.isEmpty(doc)) {
+      next({
+        statusCode: 409,
+        body: { nick: env.helpers.t('users.auth.reg_form.errors.nick_exists')}
+      });
+      return;
+    }
+    next();
+
+  });
+};
