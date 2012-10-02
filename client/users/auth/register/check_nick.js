879455cc5faf347d88581161085c65882fe3d6e6
Eugene Shkuropat <e.shkuropat@gmail.com>
2012-10-02 14:35:42 +0400

    Add delay on nick checking request

--- client/users/auth/register/check_nick.js
+++ client/users/auth/register/check_nick.js
@@ -20,6 +20,9 @@
 
 /*global nodeca*/
 
+var DELAY = 500;
+
+var start_point;
 
 /**
  *  client.common.auth.register.check_nick($elem, event)
@@ -27,35 +30,50 @@
  *  send nick value on server
  *  and show error if nick exists
  **/
-module.exports = function ($elem /*, event*/) {
-  var nick = $elem.val();
+module.exports = function ($elem, event) {
+  // update start point each time
+  start_point = new Date();
 
-  nodeca.server.users.auth.register.check_nick({ nick: nick }, function(err){
-    var $control_group = $elem.parents('.control-group:first');
+  // delay request
+  setTimeout(function() {
 
-    if (err) {
-      // Problems with nick
-      if (err.statusCode === nodeca.io.BAD_REQUEST) {
-        $control_group.addClass('error');
-
-        $control_group.find('.help-block').text(
-          err.message['nick']
-        );
-        return;
-      }
-
-      // something fatal
-      nodeca.client.common.notify('error', err.message);
+    // time is not come
+    // new event(s) update start point
+    if (DELAY > new Date() - start_point) {
       return;
     }
 
-    // no errors -> restore defaults
+    // reset time
+    start_point = null;
 
-    $control_group.removeClass('error');
-    $control_group.find('.help-block').text(
-      nodeca.runtime.t('users.auth.reg_form.nick_help')
-    );
+    var nick = $elem.val();
+    nodeca.server.users.auth.register.check_nick({ nick: nick }, function(err){
+      var $control_group = $elem.parents('.control-group:first');
 
-  });
+      if (err) {
+        // Problems with nick
+        if (err.statusCode === nodeca.io.BAD_REQUEST) {
+          $control_group.addClass('error');
+
+          $control_group.find('.help-block').text(
+            err.message['nick']
+            );
+          return;
+        }
+
+        // something fatal
+        nodeca.client.common.notify('error', err.message);
+        return;
+      }
+
+      // no errors -> restore defaults
+
+      $control_group.removeClass('error');
+      $control_group.find('.help-block').text(
+        nodeca.runtime.t('users.auth.reg_form.nick_help')
+      );
+
+    });
+  }, DELAY);
   return false;
 };
