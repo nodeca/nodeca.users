089e9fa3587636c62c34a4b62338b2f3bc284e6b
Eugene Shkuropat <e.shkuropat@gmail.com>
2012-10-09 15:56:14 +0400

    Implement client part of delete usergroup action

--- client/admin/users/usergroups/delete.js
+++ client/admin/users/usergroups/delete.js
@@ -17,13 +17,23 @@
  *  client.admin.usergroups.delete
  **/
 
+/*global nodeca, window*/
 
 /**
  *  client.admin.usergroups.delete($elem, event)
  *
  **/
-module.exports = function () {
-  // FIXME Implement me
+module.exports = function ($elem) {
+  var params = { short_name: $elem.attr('id') };
+  nodeca.server.admin.users.usergroups.delete(params, function(err){
+    if (err) {
+      // something wrong
+      nodeca.client.admin.notify('error', err.message);
+      return;
+    }
+  });
+
+  window.location = nodeca.runtime.router.linkTo('admin.users.usergroups.index');
 
   // Disable regular click
   return false;
