2dab994251ef442996d2b6f8c33e33297060eb9b
Eugene Shkuropat <e.shkuropat@gmail.com>
2012-10-22 18:40:22 +0400

    Fixed property name in usergroups model

--- models/users/UserGroup.js
+++ models/users/UserGroup.js
@@ -36,7 +36,7 @@ var UserGroup = module.exports.UserGroup = new Schema({
   , parent            : Schema.Types.ObjectId
 
     // can by deleted?
-  , protected         : { type: Boolean, default: false }
+  , is_protected         : { type: Boolean, default: false }
 
     // belong to only this group settings (overriden)
   , raw_settings      : { type: Schema.Types.Mixed, default: {}}
