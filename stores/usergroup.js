44cc5b736dc0d2313381aa6e8f3c3c28017f0b51
Aleksey V Zapparov <ixti@member.fsf.org>
2012-10-23 23:32:31 +0200

    Fix usergroup usage

--- stores/usergroup.js
+++ stores/usergroup.js
@@ -9,14 +9,14 @@ var Store = require('nlib').Settings.Store;
 
 
 var fetch_usergroups = nodeca.components.memoizee(function (ids, callback) {
-  nodeca.models.UserGroup.find().where('_id').in(ids).exec(callback);
+  nodeca.models.users.UserGroup.find().where('_id').in(ids).exec(callback);
 }, { async: true, maxAge: 30000 });
 
 
 ////////////////////////////////////////////////////////////////////////////////
 
 
-var UsergroupStore = new Store({
+var UserGroupStore = new Store({
   get: function (key, params, options, callback) {
     fetch_usergroups(params.usergroup_ids, function (err, ugs) {
       if (err) {
@@ -24,9 +24,16 @@ var UsergroupStore = new Store({
         return;
       }
 
-      var values = ugs.map(function (ug) { return ug.settings[key]; });
+      var values = ugs.map(function (ug) {
+        // map to the list of settings key value
+        return ug.settings[key];
+      }).filter(function (val) {
+        // leave only those who have ones
+        return !!val;
+      });
 
-      values.push( UsergroupStore.getDefaultValue(key) );
+      // push default value
+      values.push({ value: UserGroupStore.getDefaultValue(key) });
 
       callback(null, Store.mergeValues(values));
     });
@@ -43,4 +50,4 @@ var UsergroupStore = new Store({
 ////////////////////////////////////////////////////////////////////////////////
 
 
-module.exports = UsergroupStore;
+module.exports = UserGroupStore;
