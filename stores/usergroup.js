6c533f0aa9e6174004d6b4af97db61df97c9572f
Aleksey V Zapparov <ixti@member.fsf.org>
2012-10-23 23:18:13 +0200

    Implement user groups

--- stores/usergroup.js
+++ stores/usergroup.js
@@ -8,17 +8,34 @@
 var Store = require('nlib').Settings.Store;
 
 
+var fetch_usergroups = nodeca.components.memoizee(function (ids, callback) {
+  nodeca.models.UserGroup.find().where('_id').in(ids).exec(callback);
+}, { async: true, maxAge: 30000 });
+
+
 ////////////////////////////////////////////////////////////////////////////////
 
 
 var UsergroupStore = new Store({
   get: function (key, params, options, callback) {
-    callback(null, {
-      value: UsergroupStore.getDefaultValue(key)
+    fetch_usergroups(params.usergroup_ids, function (err, ugs) {
+      if (err) {
+        callback(err);
+        return;
+      }
+
+      var values = ugs.map(function (ug) { return ug.settings[key]; });
+
+      values.push( UsergroupStore.getDefaultValue(key) );
+
+      callback(null, Store.mergeValues(values));
     });
   },
   set: function (values, params, callback) {
     callback('Not implemented yet');
+  },
+  params: {
+    usergroup_ids: { type: 'array', required: true }
   }
 });
 
