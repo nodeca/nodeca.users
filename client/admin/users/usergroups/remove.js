da4a9a3f66cc1b39de0a89958049218d24430ded
Eugene Shkuropat <e.shkuropat@gmail.com>
2012-10-24 20:22:56 +0400

    Fixed delete usergroup errors display

--- client/admin/users/usergroups/remove.js
+++ client/admin/users/usergroups/remove.js
@@ -32,8 +32,14 @@ module.exports = function ($elem) {
 
   nodeca.server.admin.users.usergroups.remove(params, function (err) {
     if (err) {
+      // something fatal
+      if (err.code === nodeca.io.BAD_REQUEST) {
+        nodeca.client.admin.notify('error', err.data.common);
+        return;
+      }
       // no need for fatal errors notifications as it's done by io automagically
       nodeca.console.error(err);
+      return;
     }
 
     window.location = nodeca.runtime.router.linkTo('admin.users.usergroups.index');
