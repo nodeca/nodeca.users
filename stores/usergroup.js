a5f04900ad044635bf20104a3ffbf796b904fa78
Aleksey V Zapparov <ixti@member.fsf.org>
2012-10-25 13:05:26 +0200

    Improve userstore

--- stores/usergroup.js
+++ stores/usergroup.js
@@ -14,14 +14,17 @@ var async = require('nlib').Vendor.Async;
 
 // Helper to fetch usergroups by IDs
 //
-function fetchUserGroups(ids, callback) {
-  nodeca.models.users.UserGroup.find().where('_id').in(ids).exec(callback);
+function fetchUsrGrpSettings(ids, callback) {
+  nodeca.models.users.UserGroup.find()
+    .select('settings')
+    .where('_id').in(ids)
+    .exec(callback);
 }
 
 
 // Memoized version of fetchUserGroups helper
 //
-var fetchUserGroupsCached = nodeca.components.memoizee(fetchUserGroups, {
+var fetchUsrGrpSettingsCached = nodeca.components.memoizee(fetchUsrGrpSettings, {
   // momoizee options. revalidate cache after 30 sec
   async:  true,
   maxAge: 30000
@@ -33,7 +36,7 @@ var fetchUserGroupsCached = nodeca.components.memoizee(fetchUserGroups, {
 
 var UserGroupStore = new Store({
   get: function (key, params, options, callback) {
-    var func = options.skipCache ? fetchUserGroups : fetchUserGroupsCached;
+    var func = options.skipCache ? fetchUsrGrpSettings : fetchUsrGrpSettingsCached;
 
     func(params.usergroup_ids, function (err, grps) {
       if (err) {
@@ -41,14 +44,12 @@ var UserGroupStore = new Store({
         return;
       }
 
-      var values = grps.map(function (grp) {
-        var value = grp.settings && grp.settings[key];
+      var values = [];
 
-        // map to the list of settings key value
-        return undefined === value ? null : value;
-      }).filter(function (value) {
-        // leave only those who have ones
-        return !!value;
+      grps.forEach(function (grp) {
+        if (grp.settings && grp.settings[key]) {
+          values.push(grp.settings[key]);
+        }
       });
 
       // push default value
@@ -58,7 +59,7 @@ var UserGroupStore = new Store({
     });
   },
   set: function (values, params, callback) {
-    fetchUserGroups(params.usergroup_ids, function (err, groups) {
+    fetchUsrGrpSettings(params.usergroup_ids, function (err, grps) {
       if (err) {
         callback(err);
         return;
@@ -68,17 +69,18 @@ var UserGroupStore = new Store({
       values = _.pick(values || {}, UserGroupStore.keys);
 
       // set values for each usergroup
-      async.forEach(groups, function (group, next) {
-        if (!group.settings) {
-          group.settings = {};
-        }
+      async.forEach(grps, function (grp, next) {
+        grp.settings = grp.settings || {};
 
         _.each(values, function (opts, key) {
-          group.settings[key] = { value: opts.value, force: !!opts.value };
+          grp.settings[key] = {
+            value: opts.value,
+            force: !!opts.value
+          };
         });
 
-        group.markModified('settings');
-        group.save(next);
+        grp.markModified('settings');
+        grp.save(next);
       }, callback);
     });
   },
