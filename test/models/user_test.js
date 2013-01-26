9d02fcb2a1676b4727c39cab5c9498ffc7dfab5c
Vitaly Puzrin <vitaly@rcdesign.ru>
2013-01-26 14:02:11 +0400

    fixed tests

--- test/models/user_test.js
+++ test/models/user_test.js
@@ -1,37 +1,40 @@
 'use strict';
 
 
-/*global nodeca, describe, it*/
+/*global N, describe, it*/
 
 
-var User = nodeca.models.users.User;
+var expect = require('chai').expect;
+
+
+var User = N.models.users.User;
 
 
 describe('User', function () {
   describe('.validateNick()', function () {
     it('should require nickname to have at least 3 chars', function () {
-      User.validateNick('ab').should.equal(false);
-      User.validateNick('abc').should.equal(true);
+      expect(User.validateNick('ab')).to.equal(false);
+      expect(User.validateNick('abc')).to.equal(true);
     });
 
     it('should allow only letters, digits, dashes and underscores', function () {
-      User.validateNick('o.O').should.equal(false);
-      User.validateNick('-O_0-').should.equal(true);
+      expect(User.validateNick('o.O')).to.equal(false);
+      expect(User.validateNick('-O_0-')).to.equal(true);
     });
   });
 
   describe('.validatePassword()', function () {
     it('should require password to have least 8 chars', function () {
-      User.validatePassword('abcd123').should.equal(false);
-      User.validatePassword('abcd1234').should.equal(true);
+      expect(User.validatePassword('abcd123')).to.equal(false);
+      expect(User.validatePassword('abcd1234')).to.equal(true);
     });
 
     it('should require password to have at least one digit and one letter', function () {
-      User.validatePassword('abcdefgh').should.equal(false);
-      User.validatePassword('abcdefg8').should.equal(true);
+      expect(User.validatePassword('abcdefgh')).to.equal(false);
+      expect(User.validatePassword('abcdefg8')).to.equal(true);
 
-      User.validatePassword('12345678').should.equal(false);
-      User.validatePassword('1234567h').should.equal(true);
+      expect(User.validatePassword('12345678')).to.equal(false);
+      expect(User.validatePassword('1234567h')).to.equal(true);
     });
   });
 });
