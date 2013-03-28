67c82ff34cb5179d66b20f32f1f4a20b11102837
Dervus Grim <dervus@lavabit.com>
2013-03-28 17:29:46 +0100

    Refactor client script of the login page.

--- client/users/auth/register/show.js
+++ client/users/auth/register/show.js
@@ -1,15 +1,17 @@
-/**
- *  Send registration data on server.
- **/
-
-
 'use strict';
 
 
 var getFormData = require('nodeca.core/lib/client/get_form_data');
 
 
-N.wire.on(module.apiPath, function register(event) {
+var CHECK_NICK_DELAY = 500;
+var checkNickTimeout;
+
+
+//
+// Send registration data on server.
+//
+N.wire.on('users.auth.register.exec', function register(event) {
   var $form  = $(event.currentTarget)
     , params = getFormData($form);
 
@@ -29,4 +31,40 @@ N.wire.on(module.apiPath, function register(event) {
 
     window.location = N.runtime.router.linkTo('users.auth.register.success');
   });
+});
+
+
+//
+// Send nick value on server and show error if nick exists
+//
+N.wire.on('users.auth.register.check_nick', function register_check_nick(event) {
+  var $elem = $(event.currentTarget);
+
+  // make sure previous timeout was cleared
+  clearTimeout(checkNickTimeout);
+
+  // delay request
+  checkNickTimeout = setTimeout(function () {
+    var nick = $elem.val();
+
+    N.io.rpc('users.auth.register.check_nick', { nick: nick }, function (err) {
+      var $control_group = $elem.parents('.control-group:first');
+
+      if (err) {
+        if (N.io.BAD_REQUEST === err.code) {
+          // Problems with nick
+          $control_group.addClass('error');
+          $control_group.find('.help-block').text(err.message['nick']);
+        } else {
+          // something fatal
+          N.wire.emit('notify', err.message);
+        }
+        return;
+      }
+
+      // no errors -> restore defaults
+      $control_group.removeClass('error');
+      $control_group.find('.help-block').text(t('nick_help'));
+    });
+  }, CHECK_NICK_DELAY);
 });
