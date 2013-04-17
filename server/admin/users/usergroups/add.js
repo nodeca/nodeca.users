a1b2e71d3a2f9296e6f7a89b8b373c67952e5931
Dervus Grim <dervus@lavabit.com>
2013-04-17 20:52:42 +0200

    Update paths.

--- server/admin/users/usergroups/add.js
+++ server/admin/users/usergroups/add.js
@@ -15,20 +15,7 @@ module.exports = function (N, apiPath) {
   N.wire.on(apiPath, function (env, callback) {
     var UserGroup = N.models.users.UserGroup;
 
-    var settings = N.config.setting_schemas['usergroup'];
-    var settings_categories = {};
-
-    // collect usergroups items and group it by setting category
-    _.keys(settings).forEach(function(name) {
-      var item = settings[name];
-      var category_name = item['category'];
-      if (!settings_categories[category_name]) {
-        settings_categories[category_name] = {};
-      }
-      settings_categories[category_name][name] = item;
-    });
-
-    env.data.settings_categories = settings_categories;
+    env.data.settings_categories = N.settings.getStore('usergroup').getCategories();
 
     // collect existing usergroups for `parent group` select
     env.data.usergroups = [];
