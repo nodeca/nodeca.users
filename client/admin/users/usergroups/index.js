3dd272c849ac2867bb57c49fc1dc4c0a59d48dd3
Dervus Grim <dervus@lavabit.com>
2013-05-10 22:15:42 +0200

    Ask for a confirmation before deleting usergroups.

--- client/admin/users/usergroups/index.js
+++ client/admin/users/usergroups/index.js
@@ -5,12 +5,14 @@ N.wire.on('admin.users.usergroups.remove', function (event) {
   var $element = $(event.currentTarget)
     , _id      = $element.data('usergroupId');
 
-  N.io.rpc('admin.users.usergroups.remove', { _id: _id }, function (err) {
-    if (err) {
-      N.wire.emit('notify', { type: 'error', message: t('error_remove') });
-      return;
-    }
+  if (window.confirm(t('remove_confirm', { group_name: $element.data('usergroupName') }))) {
+    N.io.rpc('admin.users.usergroups.remove', { _id: _id }, function (err) {
+      if (err) {
+        N.wire.emit('notify', { type: 'error', message: t('error_remove') });
+        return;
+      }
 
-    $element.parents('tr').remove();
-  });
+      $element.parents('tr').remove();
+    });
+  }
 });
