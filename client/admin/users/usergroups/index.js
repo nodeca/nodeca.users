264e56df48e49c6e7ed8198b1c2cdfa52eaab6bb
Dervus Grim <dervus@lavabit.com>
2013-05-13 21:49:18 +0200

    Correct translation names.

--- client/admin/users/usergroups/index.js
+++ client/admin/users/usergroups/index.js
@@ -5,10 +5,10 @@ N.wire.on('admin.users.usergroups.destroy', function (event) {
   var $element = $(event.currentTarget)
     , _id      = $element.data('usergroupId');
 
-  if (window.confirm(t('destroy_confirm', { group_name: $element.data('usergroupName') }))) {
+  if (window.confirm(t('delete_confirm', { group_name: $element.data('usergroupName') }))) {
     N.io.rpc('admin.users.usergroups.destroy', { _id: _id }, function (err) {
       if (err) {
-        N.wire.emit('notify', { type: 'error', message: t('error_destroy') });
+        N.wire.emit('notify', { type: 'error', message: t('error_delete') });
         return;
       }
 
