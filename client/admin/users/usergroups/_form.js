d7edb89766ec3b579d36d1abf177b4667e0ae13d
Vitaly Puzrin <vitaly@rcdesign.ru>
2013-05-26 23:57:18 +0400

    lint fix

--- client/admin/users/usergroups/_form.js
+++ client/admin/users/usergroups/_form.js
@@ -180,11 +180,12 @@ function UserGroup(form, data) {
   // Computed values.
   //
   this.localizedName = ko.computed(function () {
-    return (this.name() && N.runtime.t.exists('admin.users.usergroup_names.' + this.name()))
-      ?
+    return (
+      (this.name() && N.runtime.t.exists('admin.users.usergroup_names.' + this.name())) ?
         N.runtime.t('admin.users.usergroup_names.' + this.name())
       :
-        null;
+        null
+    );
   }, this);
 
   this.parentGroup = ko.computed({
