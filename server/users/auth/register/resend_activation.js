d93fb1149308eb606e0d0c7870c3c5b00e43f2e2
Dervus Grim <dervus@lavabit.com>
2013-05-27 17:16:37 +0200

    Drop `env.runtime.is_validating` variable.

--- server/users/auth/register/resend_activation.js
+++ server/users/auth/register/resend_activation.js
@@ -13,47 +13,63 @@ module.exports = function (N, apiPath) {
   N.validate(apiPath, {});
 
 
+  N.wire.before(apiPath, function find_validating_group_id(env, callback) {
+    N.models.users.UserGroup.findIdByName('validating', function(err, id) {
+      env.data.validatingGroupId = id;
+      callback(err);
+    });
+  });
+
+
   N.wire.on(apiPath, function resend_activation(env, callback) {
     env.response.data.head.title = env.helpers.t('users.auth.register.title');
 
-    if (!env.runtime.is_member || !env.session.user_id) {
+    if (!env.session.user_id) {
       callback(N.io.NOT_AUTHORIZED);
       return;
     }
 
-    if (!env.runtime.is_validating) {
-      callback({
-        code: N.io.REDIRECT
-      , head: { Location: N.runtime.router.linkTo('users.profile') }
-      });
-      return;
-    }
-
-    N.models.users.AuthLink
-        .findOne({ user_id: env.session.user_id })
-        .setOptions({ lean: true })
-        .exec(function (err, authlink) {
-
+    N.models.users.User.findById(env.session.user_id, function (err, user) {
       if (err) {
         callback(err);
         return;
       }
 
-      if (!authlink) {
-        callback(N.io.NOT_AUTHORIZED);
+      // Reject request if current user isn't in 'validating' group.
+      if (-1 === user.usergroups.indexOf(env.data.validatingGroupId)) {
+        callback({
+          code: N.io.REDIRECT
+        , head: { Location: N.runtime.router.linkTo('users.profile') }
+        });
         return;
       }
 
-      var plainProvider = _.find(authlink.providers, function (provider) {
-        return 'plain' === provider.type;
+      N.models.users.AuthLink
+          .findOne({ user_id: user._id })
+          .setOptions({ lean: true })
+          .exec(function (err, authlink) {
+
+        if (err) {
+          callback(err);
+          return;
+        }
+
+        if (!authlink) {
+          callback(N.io.NOT_AUTHORIZED);
+          return;
+        }
+
+        var plainProvider = _.find(authlink.providers, function (provider) {
+          return 'plain' === provider.type;
+        });
+
+        if (!plainProvider) {
+          callback(N.io.NOT_AUTHORIZED);
+          return;
+        }
+
+        sendActivationToken(N, env, plainProvider.email, callback);
       });
-
-      if (!plainProvider) {
-        callback(N.io.NOT_AUTHORIZED);
-        return;
-      }
-
-      sendActivationToken(N, env, plainProvider.email, callback);
     });
   });
 };
