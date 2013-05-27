0e9214badf3240a533765bbd8261925083e78a6b
Vitaly Puzrin <vitaly@rcdesign.ru>
2013-05-28 03:33:26 +0400

    merged migration scripts

--- db/migrate/20121008120558_create_usergroups.js
+++ db/migrate/20121008120558_create_usergroups.js
@@ -6,7 +6,7 @@ var async = require('async');
 
 exports.up = function (N, callback) {
   // FIXME implement sets of real group items
-  async.forEachSeries(['administrators', 'members', 'guests'], function(name, next) {
+  async.forEachSeries(['administrators', 'guests', 'validating', 'members'], function(name, next) {
     var usergroup = new N.models.users.UserGroup({
       short_name:   name
     , is_protected: true
