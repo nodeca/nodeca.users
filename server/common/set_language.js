32ce1139b73c4b0f8c53ddc82732f1438f94b109
Dervus Grim <dervus@lavabit.com>
2013-05-29 18:36:01 +0200

    set_language: use default locale if user sent something wrong.

--- server/common/set_language.js
+++ server/common/set_language.js
@@ -5,25 +5,32 @@
 'use strict';
 
 
+var _ = require('lodash');
+
+
 var LOCALE_COOKIE_MAX_AGE = 0xFFFFFFFF; // Maximum 32-bit unsigned integer.
 
 
 module.exports = function (N, apiPath) {
   N.validate(apiPath, {
-    locale: {
-      type: 'string'
-    , enum: N.config.locales.enabled
-    }
+    locale: { type: 'string' }
   });
 
   N.wire.on(apiPath, function set_language(env, callback) {
-    env.extras.setCookie('locale', env.params.locale, {
+    var locale = env.params.locale;
+
+    if (!_.contains(N.config.locales.enabled, env.params.locale)) {
+      // User sent a non-existent or disabled locale - reply with the default.
+      locale = N.config.locales['default'];
+    }
+
+    env.extras.setCookie('locale', locale, {
       path: '/'
     , maxAge: LOCALE_COOKIE_MAX_AGE
     });
 
     if (env.session) {
-      env.session.locale = env.params.locale;
+      env.session.locale = locale;
     }
 
     if (env.session && env.session.user_id) {
@@ -33,7 +40,7 @@ module.exports = function (N, apiPath) {
           return;
         }
 
-        user.locale = env.params.locale;
+        user.locale = locale;
         user.save(callback);
       });
     } else {
