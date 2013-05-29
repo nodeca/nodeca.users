535099fd53675b1ca9781ae0ed8c87a03f78a44a
Dervus Grim <dervus@lavabit.com>
2013-05-29 22:26:12 +0200

    Drop 'auth_logout_redirect_url' setting and 'auth' setting group.

--- server/users/auth/logout.js
+++ server/users/auth/logout.js
@@ -7,17 +7,7 @@
 module.exports = function (N, apiPath) {
   N.validate(apiPath, {});
 
-  N.wire.on(apiPath, function (env, callback) {
+  N.wire.on(apiPath, function (env) {
     env.session = null;
-
-    N.settings.get('auth_logout_redirect_url', {}, function (err, url) {
-      if (err) {
-        callback(err);
-        return;
-      }
-
-      env.response.data.redirect_url = url;
-      callback();
-    });
   });
 };
