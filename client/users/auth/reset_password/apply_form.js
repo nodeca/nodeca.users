6272446e82ff5d732849d57552b752e07b45b162
Dervus Grim <dervus@lavabit.com>
2013-06-03 17:09:11 +0200

    Use Knockout on reset password apply form.

--- client/users/auth/reset_password/apply_form.js
+++ client/users/auth/reset_password/apply_form.js
@@ -1,19 +1,35 @@
 'use strict';
 
 
+var ko          = require('knockout');
 var getFormData = require('nodeca.core/lib/client/get_form_data');
+
+
+var view = null;
+
+
+N.wire.on('navigate.done:' + module.apiPath, function setup_page() {
+  view = { status: ko.observable(null) };
+  ko.applyBindings(view, $('#content')[0]);
+});
+
+
+N.wire.on('navigate.exit:' + module.apiPath, function teardown_page() {
+  ko.cleanNode($('#content')[0]);
+  view = null;
+});
 
 
 N.wire.on('users.auth.reset_password.apply', function reset_password(event) {
   var $form = $(event.currentTarget);
 
   N.io.rpc('users.auth.reset_password.apply', getFormData($form), function (err) {
-    if (err && N.io.CLIENT_ERROR === err.code && err.bad_password) {
-      $form.find('input').parents('.control-group:first').addClass('error');
+    if (err && N.io.CLIENT_ERROR === err.code) {
+      view.status(err.bad_password ? 'error' : null);
       return;
     }
 
-    if (err) {
+    if (err && err.message) {
       return false;
     }
 
