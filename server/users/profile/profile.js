3158c8ecc04eb5bc783b6422c542fc4a94c45a84
Kirill Efimov <kirill89@gmail.com>
2014-06-05 15:58:14 +0400

    Added redirect from profile page to member page

--- server/users/profile/profile.js
+++ server/users/profile/profile.js
@@ -2,13 +2,34 @@
 
 
 module.exports = function (N, apiPath) {
-  N.validate(apiPath, {
+  var User = N.models.users.User;
+
+  N.validate(apiPath, {});
+
+
+  // Check auth
+  //
+  N.wire.before(apiPath, function check_user_auth(env) {
+    if (env.user_info.is_guest) {
+      return N.io.NOT_AUTHORIZED;
+    }
   });
 
-  // Request handler
+
+  // Get current user hid and redirect to albums page
   //
-  N.wire.on(apiPath, function (env, callback) {
-    //FIXME implement me
-    callback();
+  N.wire.on(apiPath, function redirect_to_albums(env, callback) {
+    User.findOne({ '_id': env.session.user_id }, function (err, user) {
+      if (err) {
+        callback(err);
+      }
+
+      callback({
+        code: N.io.REDIRECT,
+        head: {
+          'Location': N.runtime.router.linkTo('users.member', { 'user_hid': user.hid })
+        }
+      });
+    });
   });
 };
