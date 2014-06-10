c2b72690991607ba2ca9c20db8a1f5c35a67f21a
Kirill Efimov <kirill89@gmail.com>
2014-06-10 18:12:57 +0400

    Added new permissions

---
+++ db/migrate/20140610182115_perm_users.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var _     = require('lodash');
+var async = require('async');
+var updateStoreSettings = require('../../server/admin/users/usergroups/_lib/update_store_settings');
+
+module.exports.up = function (N, cb) {
+  var models = N.models;
+
+  async.series([
+    //add usergroup settings for administrators, members
+    function (callback) {
+      models.users.UserGroup.find({ short_name: { $in: ['administrators', 'members'] } })
+        .exec(function (err, groups) {
+
+          if (err) {
+            callback(err);
+            return;
+          }
+
+          async.each(groups, function (group, next) {
+            group.raw_settings = _.extend({}, group.raw_settings, {
+              users_can_see_profile: { value: true },
+              users_can_upload_media: { value: true }
+            });
+            group.save(next);
+          }, callback);
+        });
+    },
+
+    // Recalculate store settings of all groups.
+    function (callback) {
+      updateStoreSettings(N, callback);
+    }
+  ], cb);
+};
