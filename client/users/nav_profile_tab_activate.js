0e450758b248a4aba4aa57337cc60c638373b490
Vitaly Puzrin <vitaly@rcdesign.ru>
2014-06-10 23:24:15 +0400

    profile tab highlighter rename

--- client/users/nav_profile_tab_activate.js
+++ client/users/nav_profile_tab_activate.js
@@ -5,7 +5,7 @@
 
 
 // Should run after global highlighter
-N.wire.after('navigate.done', function setup_page() {
+N.wire.after('navigate.done', function nav_profile_tab_activate() {
   var profileTab = $('.nav-horiz').find('[data-api-path="users.profile_redirect"]');
 
   if (N.runtime.page_data.profile_tab) {
