82618e2a84b55b8e0dce77253ca860b54e070d50
Kirill Efimov <kirill89@gmail.com>
2014-06-23 14:11:36 +0400

    Personal progress for files in uploader dialog. Optimized uploader client code

--- client/users/uploader/uploader.js
+++ client/users/uploader/uploader.js
@@ -5,8 +5,12 @@
 
 var $uploadDialog;
 var $uploader;
-var uploadHandler;
-
+// XHR uploading handlers
+var uploadHandlers;
+var settings;
+var onDone;
+// To track uploading progress, increases for for each file
+var lastFileId = 1;
 
 // Add files to uploader
 //
@@ -21,6 +25,66 @@ N.wire.on('users.uploader:add', function add_files(files) {
 });
 
 
+var onAddFile = function (event, data) {
+  // onAddFile will be called for each file independently
+  // data.files array always contains only one item
+  var file = data.files[0];
+  data.fileId = lastFileId++;
+
+  // Check files extensions
+  var allowedFileExt = new RegExp('(.)(' + settings.allowed_extensions.join('|') + ')$', 'i');
+  if (!allowedFileExt.test(file.name)) {
+    N.wire.emit('notify', { type: 'error', message: t('unsupported_error', { 'file_name': file.name }) });
+    return;
+  }
+
+  $('<li/>')
+    .appendTo('#users-uploader__files')
+    .append('<div class="users-uploader__file">' + file.name + '</div>')
+    .append('<div class="users-uploader__bar img-rounded"><div class="users-uploader__progress"></div></div>')
+    .attr('id', 'file_' + data.fileId);
+
+  // Show progress dialog
+  var $this = $(this);
+  $uploadDialog
+    .on('shown.bs.modal.add_file', function () {
+      data.process(function () {
+        return $this.fileupload('process', data);
+      }).done(function () {
+        $uploadDialog.off('shown.bs.modal.add_file');
+        // Run query
+        uploadHandlers.push(data.submit());
+      });
+    })
+    .modal('show');
+};
+
+
+var onProgress = function (event, data) {
+  var progress = parseInt(data.loaded / data.total * 100, 10) + '%';
+  $('#file_' + data.fileId + ' .users-uploader__progress').width(progress);
+};
+
+
+var onFail = function (event, data) {
+  // User abort
+  if (data.errorThrown === 'abort') {
+    return;
+  }
+
+  N.wire.emit('notify', { type: 'error', message: t('upload_error') });
+};
+
+
+var onStop = function () {
+  uploadHandlers = [];
+  $('#users-uploader__files').empty();
+  $uploadDialog.modal('hide');
+  // Reload medias
+  N.wire.emit(onDone);
+};
+
+
 // Initialize uploader
 //
 // - params Object
@@ -29,29 +93,26 @@ N.wire.on('users.uploader:add', function add_files(files) {
 //   - onDone - name of event, emit when uploads finish
 //
 N.wire.on('users.uploader:init', function init_uploader(params) {
-  N.io.rpc('users.uploader_config', {}, function (err, settings) {
+  N.io.rpc('users.uploader_config', {}, function (err, uploaderSettings) {
     if (err) { return false; }
+    settings = uploaderSettings;
+    onDone = params.onDone;
 
-    $uploadDialog = $('#users-upload__dialog');
-    var $progressText = $('#users-upload__data');
-    var $progress = $('#users-upload__progress');
+    $uploadDialog = $('#users-uploader__dialog');
+    uploadHandlers = [];
+    $('#users-uploader__files').empty();
 
-    $uploadDialog
-      .on('hidden.bs.modal', function () {
-        // Abort query if user closes dialog
-        if (uploadHandler) {
-          uploadHandler.abort();
-        }
-      })
-      .on('shown.bs.modal', function () {
-        // Reset values
-        $progress.width('0%');
-        $progressText.text('0%');
+    $uploadDialog.on('hidden.bs.modal', function () {
+      // Abort query if user closes dialog
+      $.each(uploadHandlers, function (index, handler) {
+        handler.abort();
       });
+    });
 
     $uploader = $(params.inputSelector).fileupload({
       formData: { csrf: N.runtime.csrf },
       sequentialUploads: true,
+      singleFileUploads: true,
       url: params.uploadUrl,
       dataType: 'json',
       dropZone: null,
@@ -71,52 +132,10 @@ N.wire.on('users.uploader:init', function init_uploader(params) {
           action: 'saveImage'
         }
       ],
-      add: function (e, data) {
-        // Check files extensions
-        var hasUnsupportedFiles = false;
-        var allowedFileExt = new RegExp('(.)(' + settings.allowed_extensions.join('|') + ')$', 'i');
-        data.files.forEach(function (file) {
-          if (!allowedFileExt.test(file.name)) {
-            N.wire.emit('notify', { type: 'error', message: t('unsupported_error', { 'file_name': file.name }) });
-            hasUnsupportedFiles = true;
-          }
-        });
-        // Has unsupported files - stop uploading
-        if (hasUnsupportedFiles) {
-          return;
-        }
-
-        // Show progress dialog
-        $uploadDialog.modal('show');
-
-        var $this = $(this);
-        data.process(function () {
-          return $this.fileupload('process', data);
-        }).done(function () {
-          // Run query
-          uploadHandler = data.submit();
-        });
-      },
-      progressall: function (e, data) {
-        // Progress for all files
-        var progress = parseInt(data.loaded / data.total * 100, 10) + '%';
-        $progress.width(progress);
-        $progressText.text(progress);
-      },
-      stop: function () {
-        $uploadDialog.modal('hide');
-        // Reload medias
-        N.wire.emit(params.onDone);
-      },
-      fail: function (e, data) {
-        // User abort
-        if (data.errorThrown === 'abort') {
-          return;
-        }
-
-        $uploadDialog.modal('hide');
-        N.wire.emit('notify', { type: 'error', message: t('upload_error') });
-      }
+      add: onAddFile,
+      progress: onProgress,
+      stop: onStop,
+      fail: onFail
     });
   });
 });
