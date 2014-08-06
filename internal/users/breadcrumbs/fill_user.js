c19ee9f10e469420d014e019de6d64f66f70a69e
Kirill Efimov <kirill89@gmail.com>
2014-08-06 12:52:13 +0400

    Added avatar into breadcrumbs

--- internal/users/breadcrumbs/fill_user.js
+++ internal/users/breadcrumbs/fill_user.js
@@ -10,9 +10,10 @@ module.exports = function (N, apiPath) {
     env.data.breadcrumbs = env.data.breadcrumbs || [];
 
     env.data.breadcrumbs.push({
-      text   : env.runtime.is_member ? user.name : user.nick,
-      route  : 'users.member',
-      params : { 'user_hid': user.hid }
+      text      : env.runtime.is_member ? user.name : user.nick,
+      route     : 'users.member',
+      params    : { 'user_hid': user.hid },
+      avatar_id : env.runtime.is_member ? user.avatar_id || user.avatar_fallback : user.avatar_fallback
     });
   });
 };
