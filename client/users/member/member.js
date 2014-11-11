dbd8420c500374d032f9a4be806c60a1ac12a86b
Kirill Efimov <kirill89@gmail.com>
2014-11-11 17:35:19 +0300

    Added store and restore blocks collapse state on member page

--- client/users/member/member.js
+++ client/users/member/member.js
@@ -3,6 +3,40 @@
 
 'use strict';
 
+var _ = require('lodash');
+
+
+// Store/restore blocks collapse state
+//
+N.wire.on('navigate.done:' + module.apiPath, function store_blocks_state() {
+  var key = 'member_blocks_collapsed';
+  var collapsedBlocks;
+  var bag = new window.Bag();
+
+  // Handle show/hide events
+  $('.member-block__inner')
+    .on('shown.bs.collapse', function (event) {
+      collapsedBlocks = _.without(collapsedBlocks, $(event.target).attr('id'));
+
+      bag.set(key, collapsedBlocks);
+    })
+    .on('hidden.bs.collapse', function (event) {
+      collapsedBlocks.push($(event.target).attr('id'));
+
+      bag.set(key, collapsedBlocks);
+    });
+
+  // Restore previous state
+  bag.get(key, function (__, data) {
+    collapsedBlocks = data || [];
+
+    collapsedBlocks.forEach(function (blockID) {
+      $('#' + blockID).removeClass('in');
+    });
+  });
+});
+
+
 N.wire.once('navigate.done:' + module.apiPath, function page_once() {
 
   // Click to avatar
@@ -26,5 +60,14 @@ N.wire.once('navigate.done:' + module.apiPath, function page_once() {
       $('.member-avatar__image').attr('src', N.router.linkTo('core.gridfs', { bucket: result.avatar_id }));
       $('.member-layout').removeClass('member-layout__m-avatar-exists');
     });
+  });
+
+
+  // Page exit
+  //
+  N.wire.on('navigate.exit:' + module.apiPath, function page_exit() {
+    $('.member-block__inner')
+      .off('shown.bs.collapse')
+      .off('hidden.bs.collapse');
   });
 });
