f7df1b10aebef969ef01d10601341a6ee210a033
Kirill Efimov <kirill89@gmail.com>
2014-11-27 17:13:46 +0300

    added quota check on restore media

--- server/users/media/destroy/destroy.js
+++ server/users/media/destroy/destroy.js
@@ -3,6 +3,8 @@
 
 'use strict';
 
+var Mongoose = require('mongoose');
+
 
 module.exports = function (N, apiPath) {
 
@@ -36,6 +38,62 @@ module.exports = function (N, apiPath) {
 
         env.data.media = media;
         callback();
+      });
+  });
+
+
+  // Check quota
+  //
+  N.wire.before(apiPath, function check_quota(env, callback) {
+    // Check quota only on restore media
+    if (!env.params.restore) {
+      callback();
+      return;
+    }
+
+    var mTypes = N.models.users.MediaInfo.types;
+    var totalSize;
+
+    // Fetch user's total media size
+    //
+    // TODO: Aggregation $groups can't use coverage index.
+    // TODO: All user's medias will be selected to calculate $sum. Check performance on production
+    N.models.users.MediaInfo.aggregate(
+      [
+        {
+          $match: {
+            user_id: new Mongoose.Types.ObjectId(env.session.user_id),
+            type: { $in: mTypes.LIST_VISIBLE }
+          }
+        },
+        { $group: { _id: null, total_size: { $sum: '$file_size' } } }
+      ],
+      function (err, data) {
+
+        if (err) {
+          callback(err);
+          return;
+        }
+
+        totalSize = data[0] ? data[0].total_size : 0;
+
+        env.extras.settings.fetch('users_media_total_quota_mb', function (err, users_media_total_quota_mb) {
+
+          if (err) {
+            callback(err);
+            return;
+          }
+
+          if (users_media_total_quota_mb * 1024 * 1024 < totalSize) {
+            callback({
+              code:    N.io.CLIENT_ERROR,
+              message: env.t('err_quota_exceeded', { quota_mb: users_media_total_quota_mb })
+            });
+            return;
+          }
+
+          callback();
+        });
       });
   });
 
