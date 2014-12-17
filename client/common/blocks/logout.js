beac5e8dce312e15e246b35f0dead93644bbbbe0
Kirill Efimov <kirill89@gmail.com>
2014-12-17 16:00:40 +0300

    redirect to site root after logout

--- client/common/blocks/logout.js
+++ client/common/blocks/logout.js
@@ -3,7 +3,7 @@
 N.wire.on(module.apiPath, function logout_init() {
   N.io.rpc('users.auth.logout').done(function () {
 
-    // In order to perform logout, we must reload the page.
-    window.location.reload();
+    // In order to perform logout, we must reload page. Go to site root.
+    window.location = '/';
   });
 });
