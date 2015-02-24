4b26a43477da061c1e056507e821181cb8d76542
Alex Kocharin <alex@kocharin.ru>
2015-02-24 11:19:55 +0000

    Generate identicons on the client-side

---
+++ client/common/system/avatar_helper/avatar_helper.js
@@ -0,0 +1,81 @@
+// Register avatar helper, and display avatars on page load
+//
+
+'use strict';
+
+var _           = require('lodash');
+var identicon   = require('nodeca.users/lib/identicon');
+var avatarSizes = _.reduce('$$ JSON.stringify(N.config.users.avatars.resize) $$', function (result, val, key) {
+  result[key] = val.width;
+  return result;
+}, {});
+
+
+// Returns a list of attributes for avatar img tag.
+//
+// Usage:
+//  - avatar_helper(user [, size_name])
+//  - avatar_helper(user_id [, size_name])
+//  - avatar_helper(user_id, user [, size_name])
+//
+function avatar_helper(user_id, user, size_name) {
+  var avatar_id, src;
+
+  if (typeof(user_id) !== 'string') {
+    // avatar_helper(user, size_name)
+    size_name = user;
+    user = user_id;
+    user_id = user._id;
+  }
+
+  if (typeof(user) !== 'object') {
+    // avatar_helper(user_id, size_name)
+    size_name = user;
+    user = null;
+  }
+
+  if (user) {
+    avatar_id = user.avatar_id;
+  } else if (N && N.runtime && N.runtime.user_id === user_id) {
+    avatar_id = N.runtime.user_avatar;
+  }
+
+  if (size_name && !avatarSizes[size_name]) {
+    throw new Error('Invalid avatar size: ' + size_name);
+  }
+
+  if (avatar_id) {
+    src = N.router.linkTo('core.gridfs', { bucket: avatar_id + (size_name ? '_' + size_name : '') });
+  } else {
+    src = identicon(user_id, avatarSizes[size_name ? size_name : 'orig']);
+  }
+
+  return {
+    src: src
+  };
+}
+
+
+N.wire.once('init:assets', function avatar_helper_register() {
+  N.runtime.render.helpers.avatar = avatar_helper;
+});
+
+N.wire.once('navigate.done', function identicon_replace() {
+  $('._identicon').each(function(n, img) {
+    var $img = $(img),
+        user_id = $img.data('user-id'),
+        avatar_id = $img.data('avatar-id'),
+        size_name = $img.data('avatar-size');
+
+    if (size_name && !avatarSizes.hasOwnProperty(size_name)) {
+      throw new Error('invalid avatar size: ' + size_name);
+    }
+
+    $img.removeClass('_identicon');
+    if (avatar_id) {
+      $img.attr('src', N.router.linkTo('core.gridfs', { bucket: avatar_id + (size_name ? '_' + size_name : '') }));
+    } else {
+      $img.attr('src', identicon(user_id, avatarSizes[size_name ? size_name : 'orig']));
+    }
+  });
+});
--- client/users/member/member.js
+++ client/users/member/member.js
@@ -3,8 +3,10 @@
 
 'use strict';
 
-var _   = require('lodash');
-var Bag = require('bag.js');
+var _           = require('lodash');
+var Bag         = require('bag.js');
+var identicon   = require('nodeca.users/lib/identicon');
+var avatarWidth = '$$ JSON.stringify(N.config.users.avatars.resize.orig.width) $$';
 
 
 // Store/restore blocks collapse state
@@ -69,17 +71,19 @@ N.wire.once('navigate.done:' + module.apiPath, function page_once() {
 
   // Show delete avatar confirm
   //
-  N.wire.before('users.member:detete_avatar', function confirm_delete_avatar(data, callback) {
+  N.wire.before('users.member:delete_avatar', function confirm_delete_avatar(data, callback) {
     N.wire.emit('common.blocks.confirm', t('delete_avatar_confirm'), callback);
   });
 
 
   // Click to delete avatar button
   //
-  N.wire.on('users.member:detete_avatar', function delete_avatar() {
-    N.io.rpc('users.avatar.delete').done(function (result) {
+  N.wire.on('users.member:delete_avatar', function delete_avatar() {
+    N.io.rpc('users.avatar.delete').done(function (/* result */) {
 
-      $('.member-avatar__image').attr('src', N.router.linkTo('core.gridfs', { bucket: result.avatar_id }));
+      var $img = $('.member-avatar__image');
+      $img.attr('src', identicon($img.data('user-id'), avatarWidth));
+
       $('.member-layout').removeClass('member-layout__m-avatar-exists');
     });
   });
