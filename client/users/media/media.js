90a56a02aca5cfc1019059482e1d3966e1c3ec6e
Vitaly Puzrin <vitaly@rcdesign.ru>
2015-03-10 11:51:10 +0300

    Style: root classes rename

--- client/users/media/media.js
+++ client/users/media/media.js
@@ -16,7 +16,7 @@ N.wire.once('navigate.done:' + module.apiPath, function page_once() {
   //
   N.wire.on('users.media:delete', function media_delete(__, callback) {
     N.io.rpc('users.media.destroy', { media_id: pageParams.media_id }).done(function () {
-      $('.user-mediapage').addClass('deleted');
+      $('.user-media-root').addClass('deleted');
       callback();
     });
   });
@@ -26,7 +26,7 @@ N.wire.once('navigate.done:' + module.apiPath, function page_once() {
   //
   N.wire.on('users.media:restore', function media_restore(__, callback) {
     N.io.rpc('users.media.destroy', { media_id: pageParams.media_id, revert: true }).done(function () {
-      $('.user-mediapage').removeClass('deleted');
+      $('.user-media-root').removeClass('deleted');
       callback();
     });
   });
