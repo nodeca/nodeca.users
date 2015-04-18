3c2997aa44dc617b87564df1da169093bceddd96
Kirill Efimov <kirill89@gmail.com>
2015-04-18 23:28:15 +0700

    Use navit instead nightmare

--- test/client/users/album/create.js
+++ test/client/users/album/create.js
@@ -3,33 +3,43 @@
 'use strict';
 
 describe('Albums create dialog', function () {
-  this.timeout(20000);
-
   it('should create new album', function (done) {
     var user;
+    var paramsStack = [];
 
     TEST.browser
       // Authorize
-      .auth('users_album_create', function (usr) {
+      .do.auth('users_album_create', function (usr) {
         user = usr;
       })
 
       // Navigate to albums page
-      .goto(function () {
+      .do.open(function () {
         return TEST.N.router.linkTo('users.albums_root', { user_hid: user.hid });
       })
 
-      .evaluateAsync(function (done) {
-        TEST.N.wire.once('users.album.create:shown', { priority: 999 }, function () {
-          $('input[name="album_name"]').val('new test album!');
-          $('.modal-dialog button[type="submit"]').click();
-        });
+      // Get albums count before create new
+      .get.count('.user-albumlist li', paramsStack)
 
-        trigger('[data-on-click="users.albums_root.create_album"]', function () {
-          assert.equal($('.user-albumlist li:last .thumb-caption__line:first').text(), 'new test album!');
-          done();
-        });
-      })
+      // Click "+"
+      .do.click('[data-on-click="users.albums_root.create_album"]')
+
+      // Wait for dialog
+      .do.wait('.modal.fade.in')
+
+      // Type album's name
+      .do.type('input[name="album_name"]', 'new test album!')
+
+      // Click "OK"
+      .do.click('.modal-dialog button[type="submit"]')
+
+      // Wait page update
+      .do.wait(function (paramsStack) {
+        return document.querySelectorAll('.user-albumlist li').length === paramsStack[0] + 1;
+      }, paramsStack)
+
+      // Check that album created
+      .test.text('.user-albumlist li:last-child .thumb-caption__line:first-child', 'new test album!')
 
       // Run test
       .run(done);
