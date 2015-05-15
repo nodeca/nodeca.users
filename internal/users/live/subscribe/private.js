7f4f71be914101ff956107e7cfad6ab12d5b1848
Kirill Efimov <kirill89@gmail.com>
2015-05-15 21:59:27 +0700

    Live: `data.helpers.sessionLoad` -> `data.getSession`

--- internal/users/live/subscribe/private.js
+++ internal/users/live/subscribe/private.js
@@ -6,16 +6,21 @@
 module.exports = function (N) {
   N.wire.before('internal.live.subscribe:users.private.*', function subscribe_permission_check(data, callback) {
     // Get user ID from channel name
-    var userID = data.channel.split('.').pop();
+    var user_id = data.channel.split('.').pop();
 
     // Load session
-    data.helpers.sessionLoad(function (err) {
+    data.getSession(function (err, session) {
       if (err) {
         callback(err);
         return;
       }
 
-      if (data.session.user_id === userID) {
+      if (!session) {
+        callback();
+        return;
+      }
+
+      if (user_id && session.user_id === user_id) {
         data.allowed = true;
       }
 
