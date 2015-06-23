c6272638e07790da4b69b5c09db5768c592bfe17
Alex Kocharin <alex@kocharin.ru>
2015-06-23 14:03:40 +0000

    Switch password hash implementation to built-in

--- models/users/_lib/password.js
+++ models/users/_lib/password.js
@@ -1,20 +1,23 @@
 'use strict';
 
 var randomBytes   = require('crypto').randomBytes;
-
-// TODO: change to async crypto pbkdf2-sha256 when node 0.12 released,
-// and increase iterations to ~200000.
-var pbkdf2   = require('pbkdf2-sha256');
-var iterations = 10000;
+var pbkdf2        = require('crypto').pbkdf2;
+var iterations    = 50000;
 
 exports.hash = function generateHash(pass, callback) {
   var salt = randomBytes(16);
-  var res  = pbkdf2(new Buffer(pass), salt, iterations, 32);
 
-  callback(null,  '$pbkdf2sha256' +
-                  '$' + iterations +
-                  '$' + salt.toString('base64') +
-                  '$' + res.toString('base64'));
+  pbkdf2(new Buffer(pass), salt, iterations, 32, 'sha256', function (err, key) {
+    if (err) {
+      callback(err);
+      return;
+    }
+
+    callback(null,  '$pbkdf2sha256' +
+                    '$' + iterations +
+                    '$' + salt.toString('base64') +
+                    '$' + key.toString('base64'));
+  });
 };
 
 exports.check = function checkPassword(pass, hash, callback) {
@@ -31,5 +34,12 @@ exports.check = function checkPassword(pass, hash, callback) {
       test    = parts[3],
       keyLen  = (new Buffer(test, 'base64')).length;
 
-  callback(null, test === pbkdf2(new Buffer(pass), salt, itCount, keyLen).toString('base64'));
+  pbkdf2(new Buffer(pass), salt, itCount, keyLen, 'sha256', function (err, key) {
+    if (err) {
+      callback(err);
+      return;
+    }
+
+    callback(null, test === key.toString('base64'));
+  });
 };
