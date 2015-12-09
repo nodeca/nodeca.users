90abf7111a2aaf049a016bbc7127efccf3456446
Kirill Efimov <kirill89@gmail.com>
2015-12-09 18:10:31 +0300

    Updated queue API

--- internal/users/notify/notify.js
+++ internal/users/notify/notify.js
@@ -16,16 +16,17 @@ var _ = require('lodash');
 module.exports = function (N, apiPath) {
   N.wire.on(apiPath, function notification_add(params, callback) {
     var taskData = _.assign({}, params, { to: Array.isArray(params.to) ? params.to : [ params.to ] });
+    var worker = N.queue.worker('notify');
 
     // Cancel previous task if exists (reset delay for send)
-    N.queue.cancel('notify', N.queue.worker('notify').taskID(taskData), function (err) {
+    worker.cancel(worker.taskID(taskData), function (err) {
       if (err) {
         callback(err);
         return;
       }
 
       // Add new task with delay
-      N.queue.postpone('notify', taskData, callback);
+      worker.postpone(taskData, callback);
     });
   });
 };
