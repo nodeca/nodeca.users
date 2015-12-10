f28cfdd8d046c4485cc4c439f926fe2ffb6b4138
Kirill Efimov <kirill89@gmail.com>
2015-12-10 19:41:25 +0300

    Added group permission: `can_receive_email`

--- internal/users/notify/deliver_email.js
+++ internal/users/notify/deliver_email.js
@@ -3,12 +3,13 @@
 'use strict';
 
 
-var async = require('async');
+var async     = require('async');
+var user_info = require('nodeca.users/lib/user_info');
 
 
 module.exports = function (N) {
   N.wire.after('internal:users.notify.deliver', function notify_deliver_email(local_env, callback) {
-    var emails;
+    var emails, users_info;
 
     // TODO: filter offline users
 
@@ -38,26 +39,66 @@ module.exports = function (N) {
         });
       },
 
+      // Fetch user info
+      //
+      function (next) {
+        user_info(N, Object.keys(local_env.messages), function (err, res) {
+          if (err) {
+            next(err);
+            return;
+          }
+
+          users_info = res;
+          next();
+        });
+      },
+
       // Send via email
       //
       function (next) {
         async.each(Object.keys(local_env.messages), function (user_id, next) {
-          var data = {
-            to: emails[user_id],
-            subject: local_env.messages[user_id].subject,
-            html: local_env.messages[user_id].text
+
+          // If user have no email - skip
+          if (!emails[user_id]) {
+            next();
+            return;
+          }
+
+          var params = {
+            user_id: user_id,
+            usergroup_ids: users_info[user_id].usergroups
           };
 
-          N.mailer.send(data, function (err) {
+          N.settings.get('can_receive_email', params, {}, function (err, can_receive_email) {
             if (err) {
-              // don't return an error here
-              N.logger.error('Cannot send email to %s: %s', emails[user_id], err.message || err);
+              next(err);
+              return;
             }
 
-            next();
+            // If user group can't receive emails - skip
+            if (!can_receive_email) {
+              next();
+              return;
+            }
+
+            var data = {
+              to: emails[user_id],
+              subject: local_env.messages[user_id].subject,
+              html: local_env.messages[user_id].text
+            };
+
+            N.mailer.send(data, function (err) {
+              if (err) {
+                // don't return an error here
+                N.logger.error('Cannot send email to %s: %s', emails[user_id], err.message || err);
+              }
+
+              next();
+            });
           });
         }, next);
       }
+
     ], callback);
   });
 };
