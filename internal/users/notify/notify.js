3c1042345393bc4d975961080ee09d9a4d314a32
Vitaly Puzrin <vitaly@rcdesign.ru>
2016-01-31 04:19:53 +0300

    Promisify queue calls

--- internal/users/notify/notify.js
+++ internal/users/notify/notify.js
@@ -14,19 +14,13 @@ var _ = require('lodash');
 
 
 module.exports = function (N, apiPath) {
-  N.wire.on(apiPath, function notification_add(params, callback) {
+  N.wire.on(apiPath, function* notification_add(params) {
     var taskData = _.assign({}, params, { to: Array.isArray(params.to) ? params.to : [ params.to ] });
     var worker = N.queue.worker('notify');
 
     // Cancel previous task if exists (reset delay for send)
-    worker.cancel(worker.taskID(taskData), function (err) {
-      if (err) {
-        callback(err);
-        return;
-      }
-
-      // Add new task with delay
-      worker.postpone(taskData, callback);
-    });
+    yield worker.cancel(worker.taskID(taskData));
+    // Add new task with delay
+    yield worker.postpone(taskData);
   });
 };
