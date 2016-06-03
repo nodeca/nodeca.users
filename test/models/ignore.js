1234029884386830fe6e6a976fdbc40e9c88a3df
Alex Kocharin <alex@kocharin.ru>
2016-06-03 13:49:00 +0000

    Add a cron job to expire ignore entries

---
+++ test/models/ignore.js
@@ -0,0 +1,50 @@
+'use strict';
+
+
+const _        = require('lodash');
+const assert   = require('assert');
+const Promise  = require('bluebird');
+const co       = require('bluebird-co').co;
+const ObjectId = require('mongoose').Types.ObjectId;
+
+
+describe('Ignore', function () {
+  it('should expire ignore entries', co.wrap(function* () {
+    let id1 = new ObjectId();
+    let id2 = new ObjectId();
+    let id3 = new ObjectId();
+    let id4 = new ObjectId();
+    let id5 = new ObjectId();
+
+    yield (new TEST.N.models.users.Ignore({
+      from: id1,
+      to: id2
+    })).save();
+
+    yield (new TEST.N.models.users.Ignore({
+      from: id1,
+      to: id3,
+      expire: new Date(Date.now() - 60000)
+    })).save();
+
+    yield (new TEST.N.models.users.Ignore({
+      from: id1,
+      to: id4,
+      expire: new Date(Date.now() + 60000)
+    })).save();
+
+    yield (new TEST.N.models.users.Ignore({
+      from: id1,
+      to: id5,
+      expire: new Date(Date.now() - 120000)
+    })).save();
+
+    // Run task
+    yield TEST.N.queue.worker('ignore_expire').push();
+    yield Promise.delay(600);
+
+    let ignored_users = yield TEST.N.models.users.Ignore.find({ from: id1 }).lean(true);
+
+    assert.deepEqual(_.map(ignored_users, 'to'), [ id2, id4 ]);
+  }));
+});
