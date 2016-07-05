b2abc5f3e428b923aaf5ce42c4c657a7cf901504
Kirill Efimov <kirill89@gmail.com>
2016-07-05 17:08:39 +0300

    profile: about block update

--- client/users/member/blocks/about/about.js
+++ client/users/member/blocks/about/about.js
@@ -1,12 +1,20 @@
 // About block
 //
-
 'use strict';
+
 
 // Init about block
 //
 N.wire.once('navigate.done:users.member', function init_about() {
 
   // Init popovers
+  //
   $('.member-block__contacts-inline [data-toggle="popover"]').popover();
+
+
+  // Show more
+  //
+  N.wire.on(module.apiPath + ':show_more', function about_show_more() {
+    $('#member-about-info').addClass('member-about-info__m-show-extra');
+  });
 });
--- server/users/member/blocks/about.js
+++ server/users/member/blocks/about.js
@@ -3,7 +3,7 @@
 'use strict';
 
 
-var _ = require('lodash');
+const _ = require('lodash');
 
 
 module.exports = function (N) {
@@ -11,12 +11,18 @@ module.exports = function (N) {
   // Fill contacts
   //
   N.wire.after('server:users.member', function fill_contacts(env) {
-    _.set(env.res, 'blocks.about.messengers', N.config.users.messengers);
-    _.set(env.res, 'blocks.about.contacts', [
-      { messenger: 'skype', value: 'skype_login_example' },
-      { messenger: 'hangout', value: 'hangout@example.com' },
-      { messenger: 'jabber', value: 'jabber@example.com' },
-      { messenger: 'email', value: 'email@example.com' }
+    _.set(env.res, 'blocks.about.list', [
+      { name: 'joined', value: env.helpers.date(env.data.user.joined_ts, 'datetime') },
+      { name: 'post_count', value: 245, link: '#' },
+      { name: 'location', value: 'saint-petersburg', link: '#' },
+      { name: 'email', value: 'email@example.com', link: 'mailto:email@example.com' }
+    ]);
+
+    _.set(env.res, 'blocks.about.extra', [
+      { name: 'skype', value: 'skype_is_good', link: 'callto:skype_is_good' },
+      { name: 'jabber', value: 'jabber@example.com', link: '#' },
+      { name: 'birthday', value: '23 Feb 1083' },
+      { name: 'hobbies', value: 'Music, Books, Football, Tennis' }
     ]);
   });
 };
