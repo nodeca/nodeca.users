233623b5a121914e09fe79f42ee01492855ce668
Vitaly Puzrin <vitaly@rcdesign.ru>
2016-07-05 21:30:57 +0300

    Member page `about` animation + code cleanup

--- client/users/member/blocks/about/about.js
+++ client/users/member/blocks/about/about.js
@@ -7,14 +7,9 @@
 //
 N.wire.once('navigate.done:users.member', function init_about() {
 
-  // Init popovers
+  // Expand hidden fields
   //
-  $('.member-block__contacts-inline [data-toggle="popover"]').popover();
-
-
-  // Show more
-  //
-  N.wire.on(module.apiPath + ':show_more', function about_show_more() {
-    $('#member-about-info').addClass('member-about-info__m-show-extra');
+  N.wire.on(module.apiPath + ':expand', function about_expand() {
+    $('#member-about-info').addClass('member-about__m-expanded');
   });
 });
