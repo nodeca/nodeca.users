6840ac59feb7c2c161ee6d5c58c1655a498d6cd8
Alex Kocharin <alex@kocharin.ru>
2016-07-14 12:46:21 +0000

    Make dialog previews during rebuild

--- internal/users/message_rebuild.js
+++ internal/users/message_rebuild.js
@@ -37,5 +37,29 @@ module.exports = function (N, apiPath) {
     });
 
     yield N.models.users.DlgMessage.update({ _id: post._id }, updateData);
+
+    //
+    // If it's the last message in the dialog, rebuild preview as well
+    //
+    let dialog = yield N.models.users.Dialog.findById(post.parent);
+
+    if (dialog && dialog.cache && String(dialog.cache.last_message) === String(post._id)) {
+      let preview_data = yield N.parser.md2preview({
+        text: post.md,
+        limit: 500,
+        link2text: true
+      });
+
+      yield N.models.users.Dialog.update({ _id: post.parent }, {
+        $set: {
+          cache: {
+            last_message: post._id,
+            last_user: post.user,
+            last_ts: post.ts,
+            preview: preview_data.preview
+          }
+        }
+      });
+    }
   });
 };
