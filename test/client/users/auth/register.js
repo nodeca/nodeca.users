2e5f97ab3550444585670718a71ca7f0530d20ff
Alex Kocharin <alex@kocharin.ru>
2016-09-09 14:42:42 +0000

    Split register and reset password tests to separate chunks

--- test/client/users/auth/register.js
+++ test/client/users/auth/register.js
@@ -1,14 +1,14 @@
 'use strict';
 
 
-const simplesmtp  = require('simplesmtp');
+const assert      = require('assert');
+const co          = require('bluebird-co').co;
 const randomBytes = require('crypto').randomBytes;
+const simplesmtp  = require('simplesmtp');
+const password    = require('nodeca.users/models/users/_lib/password');
 
 
 describe('Register', function () {
-  let login    = randomBytes(10).toString('hex');
-  let email    = login + '@example.com';
-  let password = randomBytes(10).toString('hex') + 'Abc123';
   let smtp;
 
 
@@ -21,45 +21,67 @@ describe('Register', function () {
     smtp.listen(2525, done);
   });
 
+  it('should send confirmation link via email', co.wrap(function* () {
+    let login    = randomBytes(10).toString('hex');
+    let email    = login + '@example.com';
+    let pass     = randomBytes(10).toString('hex') + 'Abc123';
 
-  it('should authorize via confirmation link', function (done) {
-    let email_body = '';
-
-    smtp.on('dataReady', (connection, cb) => {
-      email_body = connection.body.replace(/\=\r\n/g, '');
-      cb();
+    let get_email = new Promise(resolve => {
+      smtp.once('dataReady', (connection, cb) => {
+        cb();
+        resolve(connection.body.replace(/\=\r\n/g, ''));
+      });
     });
 
-    function waitForEmail(cb) {
-      if (email_body) {
-        cb();
-        return;
-      }
-
-      setTimeout(() => {
-        waitForEmail(cb);
-      }, 100);
-    }
-
-    TEST.browser
+    yield TEST.browser
       // Register user
       .do.auth()
       .do.open(TEST.N.router.linkTo('users.auth.register.show'))
       .wait(500)
       .do.fill('form[data-on-submit="users.auth.register.exec"]', {
         email,
-        pass: password,
+        pass,
         nick: login
       })
       .do.click('button[type="submit"]')
-      .fn(waitForEmail)
+      .close()
+      .run();
 
+    let email_body = yield get_email;
+    let route = TEST.N.router.match(/http:\/\/localhost:3005\/[^\s]+/.exec(email_body)[0]);
+
+    assert.equal(route.meta.methods.get, 'users.auth.register.activate_exec');
+
+    let token = yield TEST.N.models.users.TokenActivationEmail.findOne({
+      secret_key: route.params.secret_key
+    });
+
+    assert.equal(token.reg_info.nick, login);
+    assert.equal(token.reg_info.email, email);
+  }));
+
+
+  it('should authorize via confirmation link', co.wrap(function* () {
+    let login = randomBytes(10).toString('hex');
+    let pass  = randomBytes(10).toString('hex') + 'Abc123';
+    let token = yield TEST.N.models.users.TokenActivationEmail.create({
+      ip: '127.0.0.1',
+      reg_info: {
+        pass_hash: password.hash(pass),
+        email:     login + '@example.com',
+        nick:      login
+      }
+    });
+
+    yield TEST.browser
       // Confirm account
-      .do.open(() => /http:\/\/localhost:3005\/[^\s]+/.exec(email_body)[0])
+      .do.open(() => TEST.N.router.linkTo('users.auth.register.activate_exec', {
+        secret_key: token.secret_key
+      }))
       .test.url(TEST.N.router.linkTo('users.auth.register.activate_done'))
       .close()
-      .run(done);
-  });
+      .run();
+  }));
 
 
   after(done => smtp.end(done));
--- test/client/users/auth/reset_password.js
+++ test/client/users/auth/reset_password.js
@@ -1,16 +1,18 @@
 'use strict';
 
 
-const simplesmtp  = require('simplesmtp');
-const randomBytes = require('crypto').randomBytes;
-const co          = require('bluebird-co').co;
+const assert      = require('assert');
 const Promise     = require('bluebird');
+const co          = require('bluebird-co').co;
+const randomBytes = require('crypto').randomBytes;
+const simplesmtp  = require('simplesmtp');
 
 
 describe('Reset password', function () {
-  let login    = randomBytes(10).toString('hex');
-  let email    = login + '@example.com';
-  let password = randomBytes(10).toString('hex') + 'Abc123';
+  let login        = randomBytes(10).toString('hex');
+  let email        = login + '@example.com';
+  let old_password = randomBytes(10).toString('hex') + 'Cde123';
+  let new_password = randomBytes(10).toString('hex') + 'Abc123';
   let user;
   let smtp;
 
@@ -31,7 +33,7 @@ describe('Reset password', function () {
       last_ip: '127.0.0.1'
     });
 
-    yield authLink.setPass(randomBytes(10).toString('hex') + 'Cde123');
+    yield authLink.setPass(old_password);
     yield authLink.save();
 
     smtp = simplesmtp.createServer({ disableDNSValidation: true });
@@ -43,26 +45,15 @@ describe('Reset password', function () {
   }));
 
 
-  it('should authorize after change', function (done) {
-    let email_body = '';
-
-    smtp.on('dataReady', (connection, cb) => {
-      email_body = connection.body;
-      cb();
+  it('should send email with password reset link', co.wrap(function* () {
+    let get_email = new Promise(resolve => {
+      smtp.once('dataReady', (connection, cb) => {
+        cb();
+        resolve(connection.body.replace(/\=\r\n/g, ''));
+      });
     });
 
-    function waitForEmail(cb) {
-      if (email_body) {
-        cb();
-        return;
-      }
-
-      setTimeout(() => {
-        waitForEmail(cb);
-      }, 100);
-    }
-
-    TEST.browser
+    yield TEST.browser
       // Request password
       .do.auth()
       .do.open(TEST.N.router.linkTo('users.auth.reset_password.request_show'))
@@ -70,34 +61,59 @@ describe('Reset password', function () {
         email
       })
       .do.click('button[type="submit"]')
-      .fn(waitForEmail)
-
-      // Change password
-      .do.open(() => {
-        let url = /http:\/\/localhost:3005\/[^\s]+/.exec(email_body)[0];
-
-        email_body = '';
-        return url;
-      })
-      .do.fill('form[data-on-submit="users.auth.reset_password.change_exec"]', {
-        password
-      })
-      .do.click('button[type="submit"]')
-      .fn(waitForEmail)
-
-      // Login with new password
-      .do.auth()
-      .do.open(TEST.N.router.linkTo('users.auth.login.show'))
-      .do.fill('form[data-on-submit="users.auth.login.plain_exec"]', {
-        email_or_nick: login,
-        pass: password
-      })
-      .do.click('button[type="submit"]')
-      .do.wait('.user-member-page')
-      .test.url(TEST.N.router.linkTo('users.member', { user_hid: user.hid }))
       .close()
-      .run(done);
-  });
+      .run();
+
+    let email_body = yield get_email;
+    let route = TEST.N.router.match(/http:\/\/localhost:3005\/[^\s]+/.exec(email_body)[0]);
+
+    assert.equal(route.meta.methods.get, 'users.auth.reset_password.change_show');
+
+    let token = yield TEST.N.models.users.TokenResetPassword.findOne({
+      secret_key: route.params.secret_key
+    });
+
+    assert.equal(String(token.user), String(user._id));
+  }));
+
+
+  it('should send email after password reset', co.wrap(function* () {
+    let token = yield TEST.N.models.users.TokenResetPassword.create({
+      user: user._id,
+      ip:   '127.0.0.1'
+    });
+
+    let get_email = new Promise(resolve => {
+      smtp.once('dataReady', (connection, cb) => {
+        cb();
+        resolve(connection.body.replace(/\=\r\n/g, ''));
+      });
+    });
+
+    yield TEST.browser
+      // Change password
+      .do.open(() => TEST.N.router.linkTo('users.auth.reset_password.change_show', {
+        secret_key: token.secret_key
+      }))
+      .do.fill('form[data-on-submit="users.auth.reset_password.change_exec"]', {
+        password: new_password
+      })
+      .do.click('button[type="submit"]')
+      .close()
+      .run();
+
+    let email_body = yield get_email;
+
+    assert(email_body.indexOf(login) !== -1);
+
+    let authLink = yield TEST.N.models.users.AuthLink.findOne({
+      email,
+      type: 'plain',
+      exists: true
+    });
+
+    assert.strictEqual(yield authLink.checkPass(new_password), true);
+  }));
 
 
   after(done => smtp.end(done));
