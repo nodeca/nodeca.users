b445711dd7fbb3ebd0235f4d2bf3c01a6586b45b
Alex Kocharin <alex@kocharin.ru>
2016-09-16 12:49:34 +0000

    Change checkboxes to multiselect on user edit page in ACP

--- client/admin/users/members/edit/blocks/usergroups/usergroups.js
+++ client/admin/users/members/edit/blocks/usergroups/usergroups.js
@@ -1,18 +1,25 @@
 
 'use strict';
 
+require('bootstrap-multiselect');
 
-N.wire.before('admin.users.members.edit:submit', function get_usergroups(data) {
-  let usergroups = [];
 
-  Object.keys(data.fields).forEach(name => {
-    let m = name.match(/^usergroup-([0-9a-f]{24})$/);
-
-    if (m) {
-      usergroups.push(m[1]);
-      delete data.fields[name];
+N.wire.on('navigate.done:admin.users.members.edit', function initialize_multiselect() {
+  $('.user-edit-usergroup-select').multiselect({
+    nonSelectedText: t('none_selected'),
+    nSelectedText:   t('selected'),
+    allSelectedText: t('all_selected'),
+    buttonClass:     'btn btn-secondary user-edit-usergroup-select__btn',
+    buttonWidth:     '100%',
+    templates: {
+      li: '<li><a tabindex="0" class="dropdown-item"><label></label></a></li>'
     }
   });
+});
 
-  data.fields.usergroups = usergroups;
+
+N.wire.before('admin.users.members.edit:submit', function get_usergroups(data) {
+  data.fields.usergroups = data.$this.serializeArray()
+                               .filter(obj => obj.name === 'usergroups')
+                               .map(obj => obj.value);
 });
