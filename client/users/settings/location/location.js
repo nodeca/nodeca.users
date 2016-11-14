e68bf5b446f83370407350be606950bec97d0a91
Alex Kocharin <alex@kocharin.ru>
2016-11-14 17:50:54 +0000

    Add UI for picking coordinates on settings/location page

--- client/users/settings/location/location.js
+++ client/users/settings/location/location.js
@@ -2,6 +2,39 @@
 'use strict';
 
 let map;
+let marker;
+
+
+// Create a marker if it does not exist, set its position
+//
+function set_marker_position(lat, lng) {
+  const leaflet = require('leaflet').noConflict();
+
+  if (!map) return;
+
+  if (marker) {
+    marker.setLatLng({ lat, lng });
+    return;
+  }
+
+  // copied from leaflet.Icon.Default.prototype.options with icon urls replaced
+  let icon = leaflet.icon({
+    iconUrl:       '$$ JSON.stringify(asset_url("nodeca.core/client/vendor/leaflet/images/marker-icon.png")) $$',
+    iconRetinaUrl: '$$ JSON.stringify(asset_url("nodeca.core/client/vendor/leaflet/images/marker-icon-2x.png")) $$',
+    shadowUrl:     '$$ JSON.stringify(asset_url("nodeca.core/client/vendor/leaflet/images/marker-shadow.png")) $$',
+    iconSize:      [ 25, 41 ],
+    iconAnchor:    [ 12, 41 ],
+    popupAnchor:   [ 1, -34 ],
+    tooltipAnchor: [ 16, -28 ],
+    shadowSize:    [ 41, 41 ]
+  });
+
+  marker = leaflet.marker([], { icon, draggable: true });
+
+  marker.setLatLng({ lat, lng });
+  marker.addTo(map);
+  $('.user-settings-location__submit').prop('disabled', false);
+}
 
 
 N.wire.on('navigate.preload:' + module.apiPath, function load_deps(preload) {
@@ -17,10 +50,39 @@ N.wire.on('navigate.done:' + module.apiPath, function initialize_map() {
   leaflet.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright" target="_blank">OpenStreetMap</a> contributors'
   }).addTo(map);
+
+  map.on('click', e => set_marker_position(e.latlng.lat, e.latlng.lng));
+});
+
+
+// Save marker position on the server when user clicks "save" button
+//
+N.wire.on(module.apiPath + ':save', function save_marker() {
+  if (!marker) return;
+
+  let latlng = marker.getLatLng();
+
+  N.io.rpc('users.settings.location.update', {
+    // normalize lng into [ -180, 180 ] range, lat should be correct as is
+    latitude:  latlng.lat,
+    longitude: latlng.lng > 0 ?
+               ((latlng.lng + 180) % 360) - 180 :
+               ((latlng.lng - 180) % 360) + 180
+  }).then(() =>
+    N.wire.emit('notify', {
+      type: 'info',
+      message: t('saved')
+    })
+  ).catch(err => N.wire.emit('error', err));
 });
 
 
 N.wire.on('navigate.exit:users.settings.location', function teardown_map() {
+  if (marker) {
+    marker.remove();
+    marker = null;
+  }
+
   if (map) {
     map.remove();
     map = null;
---
+++ server/users/settings/location/update.js
@@ -0,0 +1,43 @@
+// Save user location
+//
+'use strict';
+
+
+module.exports = function (N, apiPath) {
+
+  N.validate(apiPath, {
+    latitude:  { type: 'number', minimum: -90,  maximum: 90 },
+    longitude: { type: 'number', minimum: -180, maximum: 180 }
+  });
+
+
+  // Check permissions
+  //
+  N.wire.before(apiPath, function* check_permissions(env) {
+    if (env.user_info.is_guest) {
+      throw N.io.FORBIDDEN;
+    }
+
+    let can_edit_profile = yield env.extras.settings.fetch('can_edit_profile');
+
+    if (!can_edit_profile) {
+      throw N.io.FORBIDDEN;
+    }
+  });
+
+
+  // Fetch current user
+  //
+  N.wire.before(apiPath, function* fetch_user(env) {
+    env.data.user = yield N.models.users.User.findById(env.user_info.user_id);
+
+    if (!env.data.user) throw N.io.NOT_FOUND;
+  });
+
+
+  // Save location
+  //
+  N.wire.on(apiPath, function* save_location(/* env */) {
+    // TODO
+  });
+};
