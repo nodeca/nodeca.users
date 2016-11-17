58a3b6aacae3131d2d1959875488cd1974a50a6b
Alex Kocharin <alex@kocharin.ru>
2016-11-17 17:58:24 +0000

    Make sitemap file names static, move metadata to redis

--- internal/common/sitemap/collect.js
+++ internal/common/sitemap/collect.js
@@ -10,24 +10,24 @@ const through2 = require('through2');
 module.exports = function (N, apiPath) {
 
   N.wire.on(apiPath, function get_users_sitemap(data) {
-    data.streams.push(
-      pump(
-        N.models.users.User.collection
-                           .find({ exists: true }, { hid: 1, last_active_ts: 1 })
-                           .sort({ hid: 1 })
-                           .stream(),
+    let stream = pump(
+      N.models.users.User.collection
+                         .find({ exists: true }, { hid: 1, last_active_ts: 1 })
+                         .sort({ hid: 1 })
+                         .stream(),
 
-        through2.obj(function (user, encoding, callback) {
-          this.push({
-            loc: N.router.linkTo('users.member', {
-              user_hid: user.hid
-            }),
-            lastmod: user.last_active_ts
-          });
+      through2.obj(function (user, encoding, callback) {
+        this.push({
+          loc: N.router.linkTo('users.member', {
+            user_hid: user.hid
+          }),
+          lastmod: user.last_active_ts
+        });
 
-          callback();
-        })
-      )
+        callback();
+      })
     );
+
+    data.streams.push({ name: 'users', stream });
   });
 };
