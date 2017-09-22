1fb7f8d1e8e093d96dc5e2ca3e7f9ff18886ca64
Vitaly Puzrin <vitaly@rcdesign.ru>
2017-09-22 06:57:58 +0300

    Simplify navit call

--- test/client/admin/users/usergroups/edit.js
+++ test/client/admin/users/usergroups/edit.js
@@ -5,21 +5,20 @@
 
 
 describe('ACP edit user group', function () {
-  it('should save changes', function (done) {
+  it('should save changes', function () {
     let user;
     let adminGroupId;
 
-    TEST.browser
+    return TEST.browser
       .do.auth('admin_users_usergroups_edit', usr => {
         user = usr;
       })
-      .fn(cb => TEST.N.models.users.UserGroup.findIdByName('administrators')
+      .fn(() => TEST.N.models.users.UserGroup.findIdByName('administrators')
                   .then(res => {
                     adminGroupId = res;
                     user.usergroups.push(adminGroupId);
                     return user.save();
                   })
-                  .asCallback(cb)
       )
       .do.open(() => TEST.N.router.linkTo('admin.users.usergroups.edit', { _id: adminGroupId }))
       .do.click('#setting_can_see_deleted_users')
@@ -34,6 +33,6 @@ describe('ACP edit user group', function () {
       .do.click('button.btn-primary[type="submit"]')
       .do.wait('.notification-info')
       .close()
-      .run(true, done);
+      .run(true);
   });
 });
