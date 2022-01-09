5f780e5802897b00eac8789dea3f4816a4d0754d
Vitaly Puzrin <vitaly@rcdesign.ru>
2022-01-09 19:18:10 +0300

    Fix: keep new dialog marker persistent after page change

--- client/common/blocks/unread_dialogs/unread_dialogs.js
+++ client/common/blocks/unread_dialogs/unread_dialogs.js
@@ -4,12 +4,13 @@
 N.wire.once('navigate.done', function page_setup() {
 
   if (N.runtime.unread_dialogs === true) {
-    $('.navbar').addClass('navbar__m-unread-dialogs');
+    // Pin class on <body> to make it persistent after page change
+    $('body').addClass('navbar__m-unread-dialogs');
   }
 
   // Update unread dialogs indicator
   //
   N.live.on(`private.member.${N.runtime.user_id}.unread_dialogs`, function update_unread_dialogs_flag(new_state) {
-    $('.navbar').toggleClass('navbar__m-unread-dialogs', new_state);
+    $('body').toggleClass('navbar__m-unread-dialogs', new_state);
   });
 });
