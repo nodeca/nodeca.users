d50a13cf509917a9cb9d4fc885879366d7474774
Vitaly Puzrin <vitaly@rcdesign.ru>
2022-02-04 15:33:56 +0300

    Infractions: auto-resize textarea

--- client/users/infraction_ask_about/infraction_ask_about.js
+++ client/users/infraction_ask_about/infraction_ask_about.js
@@ -27,6 +27,16 @@ N.wire.on('navigate.preload:' + module.apiPath, function load_deps(preload) {
 N.wire.on('navigate.done:' + module.apiPath, function page_setup() {
   const ko = require('knockout');
 
+  // https://stackoverflow.com/a/42298478/1031804
+  function resizeToFitContent(el) {
+    el.style.height = '1px'; el.style.height = el.scrollHeight + 'px';
+  }
+  ko.bindingHandlers.autoResize = ko.bindingHandlers.autoResize || {
+    init(element, valueAccessor) {
+      ko.computed(function () { ko.unwrap(valueAccessor()); resizeToFitContent(element); });
+    }
+  };
+
   // Root view model.
   view = {};
   view.message = new Control();
